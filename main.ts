710b9827ce1e401f632dfb915a1079bd2331b71b
MicrobitAnne <161485257+MicrobitAnne@users.noreply.github.com>
2024-04-15 11:05:36 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+})
+Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Stop)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
